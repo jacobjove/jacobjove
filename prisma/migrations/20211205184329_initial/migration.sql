487ef235d57e41565dd7cc2aa6aef95e6d10a962
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-12-05 12:43:50 -0600

    initial commit

---
+++ prisma/migrations/20211205184329_initial/migration.sql
@@ -0,0 +1,246 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Color" AS ENUM ('BLUE');
+
+-- CreateEnum
+CREATE TYPE "TYPE" AS ENUM ('CHRON', 'INTERVAL');
+
+-- CreateEnum
+CREATE TYPE "UNIT" AS ENUM ('MINUTE', 'HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "uid" TEXT NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Calendar" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" "Color",
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Calendar_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "calendarId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IdentitySelection" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "identityId" INTEGER NOT NULL,
+    "deleted" TIMESTAMP(3),
+
+    CONSTRAINT "IdentitySelection_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "identities" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "identities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IdentityActionRelation" (
+    "id" SERIAL NOT NULL,
+    "identityId" INTEGER NOT NULL,
+    "actionId" INTEGER NOT NULL,
+    "deleted" TIMESTAMP(3),
+
+    CONSTRAINT "IdentityActionRelation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ActionCompletion" (
+    "id" SERIAL NOT NULL,
+    "actionId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ActionCompletion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Schedule" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "actionId" INTEGER NOT NULL,
+    "frequency" "UNIT" NOT NULL DEFAULT E'DAY',
+    "multiplier" INTEGER NOT NULL DEFAULT 1,
+    "chron" TEXT,
+    "adoptedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "abandonedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "beliefs" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "beliefs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BeliefSelection" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "beliefId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "BeliefSelection_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "values" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "values_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ValueSelection" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "valueId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "ValueSelection_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "IdentitySelection_userId_identityId_key" ON "IdentitySelection"("userId", "identityId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "identities_slug_key" ON "identities"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "IdentityActionRelation_identityId_actionId_key" ON "IdentityActionRelation"("identityId", "actionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Action_slug_key" ON "Action"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Schedule_userId_actionId_key" ON "Schedule"("userId", "actionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "beliefs_slug_key" ON "beliefs"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BeliefSelection_userId_beliefId_key" ON "BeliefSelection"("userId", "beliefId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "values_slug_key" ON "values"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ValueSelection_userId_valueId_key" ON "ValueSelection"("userId", "valueId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Calendar" ADD CONSTRAINT "Calendar_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_calendarId_fkey" FOREIGN KEY ("calendarId") REFERENCES "Calendar"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IdentitySelection" ADD CONSTRAINT "IdentitySelection_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IdentitySelection" ADD CONSTRAINT "IdentitySelection_identityId_fkey" FOREIGN KEY ("identityId") REFERENCES "identities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IdentityActionRelation" ADD CONSTRAINT "IdentityActionRelation_identityId_fkey" FOREIGN KEY ("identityId") REFERENCES "identities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "IdentityActionRelation" ADD CONSTRAINT "IdentityActionRelation_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "Action"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActionCompletion" ADD CONSTRAINT "ActionCompletion_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "Action"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ActionCompletion" ADD CONSTRAINT "ActionCompletion_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "Action"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BeliefSelection" ADD CONSTRAINT "BeliefSelection_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BeliefSelection" ADD CONSTRAINT "BeliefSelection_beliefId_fkey" FOREIGN KEY ("beliefId") REFERENCES "beliefs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ValueSelection" ADD CONSTRAINT "ValueSelection_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ValueSelection" ADD CONSTRAINT "ValueSelection_valueId_fkey" FOREIGN KEY ("valueId") REFERENCES "values"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
