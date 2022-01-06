d2b4e88d7d057da0379a4e48eb46693619b1af8e
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-01-06 12:40:44 -0600

    initial commit

--- components/actions/ActionBox.tsx
+++ components/actions/ActionBox.tsx
@@ -46,21 +46,12 @@ export default ActionBox;
 
 const steps = [
   {
-    label: "Select campaign settings",
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
+    label: "Select actions",
+    description: `Select one or more actions.`,
   },
   {
-    label: "Create an ad group",
-    description: "An ad group contains one or more ads which target a shared set of keywords.",
-  },
-  {
-    label: "Create an ad",
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: "Add schedules",
+    description: "Add schedules for your tracked actions.",
   },
 ];
 
