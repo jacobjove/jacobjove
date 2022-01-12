a4a0ccec49ec76df8f354287e2d5d87916897745
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-01-12 17:17:27 -0600

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Layout from "@/components/Layout";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import { GetServerSideProps, NextPage } from "next";
 import { getSession } from "next-auth/react";
 import { NextSeo } from "next-seo";
@@ -19,21 +17,16 @@ const DefaultPage: NextPage<DefaultPageProps> = (props: DefaultPageProps) => {
   return (
     <Layout>
       <NextSeo title={"SelfBuilder"} canonical={"/"} description={"Be your best self."} />
-      <Container maxWidth={"lg"}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} lg={8} alignItems={"center"}>
-            <Box>
-              <Typography variant={"h1"}>Be your best self.</Typography>
-            </Box>
-            <Box marginTop="2rem">
-              <Link href={props.ctaHref} passHref>
-                <Button variant={"contained"} color={"primary"}>
-                  {props.ctaHref === "/app/dashboard" ? "Go to dashboard" : "Sign up"}
-                </Button>
-              </Link>
-            </Box>
-          </Grid>
-        </Grid>
+      <Container maxWidth={"lg"} sx={{ height: "100%" }}>
+        <Box height="100%" display="flex" alignItems="center" justifyContent={"center"}>
+          <Box marginTop="2rem">
+            <Link href={props.ctaHref} passHref>
+              <Button variant={"contained"} color={"primary"}>
+                {props.ctaHref === "/app/dashboard" ? "Go to dashboard" : "Sign up"}
+              </Button>
+            </Link>
+          </Box>
+        </Box>
       </Container>
     </Layout>
   );
