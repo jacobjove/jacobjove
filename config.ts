46eacd30afe114121c5c5f107848ec4791546f56
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-08-19 10:23:32 -0400

    refactoring

--- config.ts
+++
@@ -1,9 +0,0 @@
-export const USE_FIREBASE = process.env.NEXT_PUBLIC_USE_FIREBASE;
-
-// "@graphql-tools/batch-execute": "^8.3.1",
-// "@graphql-tools/delegate": "^8.4.2",
-// "@graphql-tools/merge": "^8.2.1",
-// "@graphql-tools/schema": "^8.3.1",
-// "@graphql-tools/url-loader": "^7.5.3",
-// "@graphql-tools/utils": "^8.5.3",
-// "@graphql-tools/wrap": "^8.3.2",
