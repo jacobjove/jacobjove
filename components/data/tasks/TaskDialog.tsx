8b820f0a577c03a8c61ccd085f4c43609563e6c0
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-08-24 03:45:18 -0400

    refactoring

--- components/data/tasks/TaskDialog.tsx
+++ components/data/tasks/TaskDialog.tsx
@@ -30,8 +30,7 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
-import { addDays, format } from "date-fns";
-import { endOfDay } from "date-fns/esm";
+import { addDays, endOfDay, format } from "date-fns";
 import {
   bindMenu,
   bindPopover,
