161464fa05c651d78965a67cac4ec31d287f4ca2
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-04 05:13:29 -0400

    Change port to 3001

--- graphql.config.ts
+++ graphql.config.ts
@@ -2,7 +2,7 @@ import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
   // schema: "graphql/schema.gql.json",
-  schema: "http://localhost:3000/api/graphql",
+  schema: "http://localhost:3001/api/graphql",
 };
 
 export default config;
