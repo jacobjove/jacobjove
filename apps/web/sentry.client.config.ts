96287c7e12a26dfdcd467285df7f7a615fa32566
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-14 16:56:26 -0400

    Add Sentry environment

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -5,11 +5,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENVIRONMENT =
+  process.env.SENTRY_ENVIRONMENT ||
+  process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ||
+  process.env.NODE_ENV;
 
 Sentry.init({
   autoSessionTracking: false,
   dsn: SENTRY_DSN || "https://c635d2ccc5de430b93f35b861d2d65ca@glitchtip.orega.org/1",
-  // Adjust this value in production, or use tracesSampler for greater control
+  environment: SENTRY_ENVIRONMENT,
   tracesSampleRate: 0.1,
   // ...
   // Note: if you want to override the automatic release value, do not set a
