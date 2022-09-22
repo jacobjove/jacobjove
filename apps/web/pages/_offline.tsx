cb34e1d5b88b9c14af0370cd8e7f99c91b9129ad
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-22 16:53:17 -0400

    Offline fallback page

--- apps/web/pages/_offline.tsx
+++ apps/web/pages/_offline.tsx
@@ -1,13 +1,36 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import AppLayout from "@web/components/AppLayout";
+import Layout from "@web/components/Layout";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
-const Fallback = () => (
-  <>
-    <Head>
-      <title>Offline</title>
-    </Head>
-    <h1>You are offline</h1>
-    <h2>Try reloading this page after establishing a network connection</h2>
-  </>
-);
+const Fallback = () => {
+  const router = useRouter();
+  const Component = router.pathname.startsWith("/app") ? AppLayout : Layout;
+  return (
+    <Component>
+      <Head>
+        <title>{"You are offline"}</title>
+      </Head>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          padding: "15%",
+        }}
+      >
+        <h1>You are offline</h1>
+        <Typography>Try reloading this page after establishing a network connection.</Typography>
+      </Box>
+    </Component>
+  );
+};
 
 export default Fallback;
