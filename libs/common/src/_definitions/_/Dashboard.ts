dc9c455a869355097b32670ace1ee2290651f44b
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-29 16:18:24 -0400

    Refactoring

--- libs/common/src/_definitions/_/Dashboard.ts
+++ libs/common/src/_definitions/_/Dashboard.ts
@@ -1,14 +1,7 @@
 import Definition, { OPTIONAL_BOOLEAN, OPTIONAL_STRING, REQUIRED_STRING } from "@common/definition";
-import { Layout as LayoutItem } from "react-grid-layout";
+// import { Layout as LayoutItem } from "react-grid-layout";
 
-type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
-
-export type CompleteDashboardLayouts = Record<
-  "xxs" | "xs" | "sm" | "md" | "lg" | "xl",
-  DashboardComponent[]
->;
-
-export type DashboardLayouts = AtLeastOne<CompleteDashboardLayouts>;
+// type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
 export type DashboardComponentKey =
   | "calendar"
@@ -19,9 +12,16 @@ export type DashboardComponentKey =
   | "routines";
 
 // https://github.com/react-grid-layout/react-grid-layout#grid-item-props
-export interface DashboardComponent extends LayoutItem {
-  i: DashboardComponentKey;
-}
+// export interface DashboardComponent extends LayoutItem {
+//   i: DashboardComponentKey;
+// }
+
+// export type CompleteDashboardLayouts = Record<
+//   "xxs" | "xs" | "sm" | "md" | "lg" | "xl",
+//   DashboardComponent[]
+// >;
+
+// export type DashboardLayouts = AtLeastOne<CompleteDashboardLayouts>;
 
 const dashboardFields = ["userId", "name", "description", "isDefault", "public"] as const;
 
