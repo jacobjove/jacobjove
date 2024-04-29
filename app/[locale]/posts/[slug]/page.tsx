fd919cbaf7caed14fc5e12846cc65dcfcdbb9849
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2024-04-29 11:12:11 -0400

    fix: static params generation

--- app/[locale]/posts/[slug]/page.tsx
+++ app/[locale]/posts/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { unstable_setRequestLocale } from 'next-intl/server';
-import type { Metadata, GetStaticPaths, GetStaticProps } from 'next';
+import type { Metadata } from 'next';
 import { serialize } from 'next-mdx-remote/serialize';
 import PostPage from './post';
 import { getStaticParams } from '@i18n';
@@ -9,27 +9,6 @@ import { getAllPostSlugs, getPostBySlug } from '@utils/blog';
 
 export const metadata: Metadata = {
   title: 'Post',
-};
-
-export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
-  const slug = params?.slug as string;
-  const { content, ...metadata } = await getPostBySlug(slug);
-  const source = await serialize(content);
-  return {
-    props: {
-      source,
-      metadata,
-    },
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const slugs = await getAllPostSlugs();
-  const paths = slugs.map((slug) => `/posts/${slug}`);
-  return {
-    paths,
-    fallback: false,
-  };
 };
 
 export default async function Page({ params }: { params: { locale: string; slug: string } }) {
@@ -44,6 +23,13 @@ export default async function Page({ params }: { params: { locale: string; slug:
   );
 }
 
-export function generateStaticParams() {
-  return getStaticParams();
+export async function generateStaticParams() {
+  const slugs = await getAllPostSlugs();
+  const locales = getStaticParams();
+  return locales.map(({ locale }) => {
+    return slugs.map((slug) => ({
+      locale,
+      slug,
+    }));
+  });
 }
